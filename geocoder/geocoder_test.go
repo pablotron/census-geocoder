fb0e74345a844d20af7c0662efe6bff4f88ddd0f
Paul Duncan <pabs@pablotron.org>
2022-10-17 01:19:09 -0400

    geocoder/geocoder_test.go: add TestGeocoderGeographies and TestGeographies

--- geocoder/geocoder_test.go
+++ geocoder/geocoder_test.go
@@ -118,8 +118,8 @@ func TestVintages(t *testing.T) {
   }
 }
 
-//go:embed testdata/data/address-matches.json
-var mockAddressMatchesJson []byte
+//go:embed testdata/data/locations.json
+var mockLocationsJson []byte
 
 func TestGeocoderLocationsFromBenchmark(t *testing.T) {
   // create mock server
@@ -131,7 +131,7 @@ func TestGeocoderLocationsFromBenchmark(t *testing.T) {
 
   // decode expected results
   var exp []AddressMatch
-  if err = json.Unmarshal(mockAddressMatchesJson, &exp); err != nil {
+  if err = json.Unmarshal(mockLocationsJson, &exp); err != nil {
     t.Fatal(err)
   }
 
@@ -157,7 +157,7 @@ func TestLocationsFromBenchmark(t *testing.T) {
 
   // decode expected results
   var exp []AddressMatch
-  if err := json.Unmarshal(mockAddressMatchesJson, &exp); err != nil {
+  if err := json.Unmarshal(mockLocationsJson, &exp); err != nil {
     t.Fatal(err)
   }
 
@@ -178,7 +178,7 @@ func TestGeocoderLocations(t *testing.T) {
 
   // decode expected results
   var exp []AddressMatch
-  if err = json.Unmarshal(mockAddressMatchesJson, &exp); err != nil {
+  if err = json.Unmarshal(mockLocationsJson, &exp); err != nil {
     t.Fatal(err)
   }
 
@@ -204,6 +204,58 @@ func TestLocations(t *testing.T) {
 
   // get locations, check for error
   _, err := Locations(testAddress)
+  if err != nil {
+    t.Fatal(err)
+  }
+}
+
+//go:embed testdata/data/geographies.json
+var mockGeographiesJson []byte
+
+func TestGeocoderGeographies(t *testing.T) {
+  testAddress := "4600 silver hill rd, 20233"
+  testBenchmark := "Public_AR_Census2020"
+  testVintage := "Census2010_Census2020"
+
+  // create mock server
+  ms, url, err := newMockServer()
+  if err != nil {
+    t.Fatal(err)
+  }
+  defer ms.Close()
+
+  // decode expected results
+  var exp []AddressMatch
+  if err = json.Unmarshal(mockGeographiesJson, &exp); err != nil {
+    t.Fatal(err)
+  }
+
+  // create geocoder
+  gc := NewGeocoder(url)
+
+  // get geographies, check for error
+  got, err := gc.Geographies(testAddress, testBenchmark, testVintage)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  // compare against expected value
+  if !reflect.DeepEqual(got, exp) {
+    t.Fatalf("got %v, exp %v", got, exp)
+  }
+}
+
+func TestGeographies(t *testing.T) {
+  testAddress := "4600 silver hill rd, 20233"
+  testBenchmark := "Public_AR_Census2020"
+  testVintage := "Census2010_Census2020"
+
+  if testing.Short() {
+    t.Skip("skipping in short mode")
+  }
+
+  // get geographies, check for error
+  _, err := Geographies(testAddress, testBenchmark, testVintage)
   if err != nil {
     t.Fatal(err)
   }
