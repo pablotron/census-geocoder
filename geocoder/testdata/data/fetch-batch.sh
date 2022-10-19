caee88c1459742aeb9e748ba44460d7e7d01e896
Paul Duncan <pabs@pablotron.org>
2022-10-19 18:05:21 -0400

    geocoder/testdata/data/fetch-batch.sh: fix input file path

--- geocoder/testdata/data/fetch-batch.sh
+++ geocoder/testdata/data/fetch-batch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ref: https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html
-curl --form addressFile=@input.csv --form benchmark=2020  https://geocoding.geo.census.gov/geocoder/locations/addressbatch --output output.csv
+exec curl -v --form addressFile=@batch-input.csv --form benchmark=2020  https://geocoding.geo.census.gov/geocoder/locations/addressbatch --output output.csv
