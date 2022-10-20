08ecebdc6d28167db10dad2abe9a70ee1c9f4b4a
Paul Duncan <pabs@pablotron.org>
2022-10-19 21:14:55 -0400

    geocoder/testdata/data: rename fetch-batch.sh to fetch-batch-locations, add comment

--- geocoder/testdata/data/fetch-batch-locations.sh
+++ geocoder/testdata/data/fetch-batch-locations.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# use curl to fetch batch locations
 # ref: https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html
+
 exec curl -v --form addressFile=@batch-input.csv --form benchmark=2020  https://geocoding.geo.census.gov/geocoder/locations/addressbatch --output output.csv
