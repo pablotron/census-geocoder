f48d939a4cce5779af75dd91ffb15a5c7df8d2b3
Paul Duncan <pabs@pablotron.org>
2022-10-23 11:08:26 -0400

    geocoder/mock_server.go: refactor

--- geocoder/mock_server.go
+++ geocoder/mock_server.go
@@ -8,69 +8,51 @@ import (
   "os"
 )
 
-// write to response writer, check for error, and log error.
-func logWrite(w http.ResponseWriter, buf []byte) {
-  if _, err := w.Write(buf); err != nil {
-    log.Print(err)
+// Add mock handler to mux at given URL path.
+func addHandler(mux *http.ServeMux, urlPath, dataPath string) error {
+  // read mock data
+  data, err := os.ReadFile(dataPath)
+  if err != nil {
+    return err
   }
+
+  // add endpoint handler
+  mux.HandleFunc(urlPath, func(w http.ResponseWriter, r *http.Request) {
+    // write response, check for error
+    if _, err := w.Write(data); err != nil {
+      // log error
+      log.Print(err)
+    }
+  })
+
+  // return success
+  return nil
 }
 
+// mock server endpoints and data
+var mockEndpoints = []struct {
+  urlPath string // url path
+  dataPath string // mock data path
+} {
+  { "/benchmarks", "testdata/responses/benchmarks.json" },
+  { "/vintages", "testdata/responses/vintages.json" },
+  { "/locations/onelineaddress", "testdata/responses/locations.json" },
+  { "/geographies/onelineaddress", "testdata/responses/geographies.json" },
+  { "/locations/addressbatch", "testdata/responses/batch-locations-2020.csv" },
+  { "/geographies/addressbatch", "testdata/responses/batch-geographies-2020-2020.csv" },
+}
+
+// Start new mock server, then return server and server URL.
 func newMockServer() (*httptest.Server, *net_url.URL, error) {
+  // create mux
   mux := http.NewServeMux()
 
-  mockBenchmarks, err := os.ReadFile("testdata/responses/benchmarks.json")
-  if err != nil {
-    return nil, nil, err
+  // add handlers
+  for _, row := range(mockEndpoints) {
+    if err := addHandler(mux, row.urlPath, row.dataPath); err != nil {
+      return nil, nil, err
+    }
   }
-
-  mux.HandleFunc("/benchmarks", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockBenchmarks)
-  })
-
-  mockVintages, err := os.ReadFile("testdata/responses/vintages.json")
-  if err != nil {
-    return nil, nil, err
-  }
-
-  mux.HandleFunc("/vintages", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockVintages)
-  })
-
-  mockLocations, err := os.ReadFile("testdata/responses/locations.json")
-  if err != nil {
-    return nil, nil, err
-  }
-
-  mux.HandleFunc("/locations/onelineaddress", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockLocations)
-  })
-
-  mockGeographies, err := os.ReadFile("testdata/responses/geographies.json")
-  if err != nil {
-    return nil, nil, err
-  }
-
-  mux.HandleFunc("/geographies/onelineaddress", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockGeographies)
-  })
-
-  mockBatchLocations, err := os.ReadFile("testdata/responses/batch-locations-2020.csv")
-  if err != nil {
-    return nil, nil, err
-  }
-
-  mux.HandleFunc("/locations/addressbatch", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockBatchLocations)
-  })
-
-  mockBatchGeographies, err := os.ReadFile("testdata/responses/batch-geographies-2020-2020.csv")
-  if err != nil {
-    return nil, nil, err
-  }
-
-  mux.HandleFunc("/geographies/addressbatch", func(w http.ResponseWriter, r *http.Request) {
-    logWrite(w, mockBatchGeographies)
-  })
 
   // create mock server
   server := httptest.NewServer(mux)
@@ -81,5 +63,6 @@ func newMockServer() (*httptest.Server, *net_url.URL, error) {
     return nil, nil, err
   }
 
+  // return server and url
   return server, url, nil
 }
